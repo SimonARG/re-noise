86db2bbda5fd0d003b46f607089704f377976a23
Simon_Paul <simonchasnovsky@gmail.com>
2024-05-01 15:36:57 -0300

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+  ? '/re-noise/' // Replace with your repository name
+  : '/',
 })

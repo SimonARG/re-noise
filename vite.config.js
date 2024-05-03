2f8c16ac37ff4266a61a503282997d1db543f709
Simon_Paul <simonchasnovsky@gmail.com>
2024-05-03 20:59:05 -0300

    Fix production asset path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
     }
   },
   base: process.env.NODE_ENV === 'production'
-  ? '/re-noise/' // Replace with your repository name
+  ? '/' // Replace with your repository name
   : '/',
 })
